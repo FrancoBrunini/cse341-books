87216152eadd9b80de151e9eddfb60d01b06241e
Franco Brunini <valentinobruni@gmail.com>
2026-09-12 12:56:49 -0300

    feat: update book collection to reference authors and finish book API

--- src/models/books.js
+++ src/models/books.js
@@ -1,5 +1,11 @@
 import { getDb } from '../db/connect.js';
 
+const authorExists = async (authorId) => {
+  const db = getDb();
+  const author = await db.collection('authors').findOne({ id: authorId });
+  return Boolean(author);
+};
+
 const getAllBooks = async () => {
   const db = getDb();
   const collection = db.collection('books');
@@ -13,5 +19,36 @@ const getBookById = async (bookId) => {
   const book = await collection.findOne({ id: bookId });
   return book;
 };
+const createBook = async (bookData) => {
+  const db = getDb();
+  const result = await db.collection('books').insertOne(bookData);
+  return { _id: result.insertedId, ...bookData };
+};
 
-export { getAllBooks, getBookById };
+const updateBook = async (id, updatedData) => {
+  const db = getDb();
+  const { id: bodyId, ...fieldsToUpdate } = updatedData;
+
+  return await db.collection('books').findOneAndUpdate(
+    { id },
+    { $set: fieldsToUpdate },
+    { returnDocument: 'after' }
+  );
+};
+
+const deleteBook = async (id) => {
+  const db = getDb();
+  const result = await db.collection('books').deleteOne({ id });
+  return result.deletedCount > 0;
+};
+
+export {
+  authorExists,
+  getAllBooks,
+  getBookById,
+  createBook,
+  updateBook,
+  deleteBook,
+};
+
+
